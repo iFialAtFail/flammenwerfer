4325bdd00f8d8d988050487abd5fe844524c5615
domatrix <domatrix6@gmail.com>
2016-10-03 20:10:51 -0400

    instantiator to XML creator. XML doc is placed in same directory as executable.

--- Flammenwerfer/Flammenwerfer/XML Creator.cs
+++ Flammenwerfer/Flammenwerfer/XML Creator.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Xml;
+using System.ComponentModel;
 
 namespace Flammenwerfer
 {
@@ -10,7 +13,17 @@ namespace Flammenwerfer
     {
         public void FileTest()
         {
-
+            XmlTextWriter xWriter = new XmlTextWriter("xDoc1.xml", Encoding.UTF8);
+            xWriter.Formatting = Formatting.Indented;
+            xWriter.WriteStartElement("Students");
+            xWriter.WriteStartElement("Student");
+            xWriter.WriteStartElement("NAME");
+            xWriter.WriteString("John");
+            xWriter.WriteEndElement();
+            xWriter.WriteEndElement();
+            xWriter.WriteEndElement();
+            xWriter.Close();
+            
         }
     }
 }
