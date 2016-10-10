6d13f35c1d6b07498abfe4a1c0b15de5ada28965
Do-Matrix <domatrix6@gmail.com>
2016-10-10 01:54:14 -0400

    Updated UserData.xml and XML Creator to fit Multiple Courses more flexibly

--- Flammenwerfer/Flammenwerfer/XML Creator.cs
+++ Flammenwerfer/Flammenwerfer/XML Creator.cs
@@ -8,9 +8,10 @@ namespace Flammenwerfer
     {
         #region variable declaration
 
-        Output displayer = new Output();
-        XMLPATH pathfilelocation = new XMLPATH();
-        string path = "";
+        Output cDisplayer = new Output();
+        XMLPATH xPathFileLocation = new XMLPATH();
+        string sPath = "";
+        int iCourseCounter = 0;
 
         #endregion
             /*Logic for creating user from prompts requested*/
@@ -19,9 +20,9 @@ namespace Flammenwerfer
         public void instantiator()
         { //entry point of User Creation logic for XML
             Console.Clear();
-            path = pathfilelocation.Path;
+            sPath = xPathFileLocation.Path;
             XmlDocument xDoc = new XmlDocument();
-            xDoc.Load(path);
+            xDoc.Load(sPath);
             CreateUser(xDoc);
         }
 
@@ -29,17 +30,25 @@ namespace Flammenwerfer
         { //user creation logic
             XmlNode xStudent = xDoc.CreateElement("Student");
             // Start Node Filler Writer
+            iCourseCounter = Int32.Parse(cDisplayer.ReadInfoDisplay("Number of courses student is taking:"));
             StudentIDNode(xDoc, xStudent);
             StudentFNNode(xDoc, xStudent);
             StudentLNNode(xDoc, xStudent);
-            CourseIDNode(xDoc, xStudent);
-            CourseNameNode(xDoc, xStudent);
-            SemesterNode(xDoc, xStudent);
-            CourseTypeNode(xDoc, xStudent);
-            CourseGradeNode(xDoc, xStudent);
+            XmlNode xCourses = xDoc.CreateElement("Courses");
+            XmlNode xCourse = xDoc.CreateElement("Course");
+            for (int iCurrentCourse = 0; iCurrentCourse < iCourseCounter; iCurrentCourse++)
+            {
+                CourseIDNode(xDoc, xStudent);
+                CourseNameNode(xDoc, xStudent);
+                SemesterNode(xDoc, xStudent);
+                CourseTypeNode(xDoc, xStudent);
+                CourseGradeNode(xDoc, xStudent);
+            }
             //End Node Filler Writer
+            xDoc.DocumentElement.AppendChild(xCourses);
+            xDoc.DocumentElement.AppendChild(xCourse);
             xDoc.DocumentElement.AppendChild(xStudent);
-            xDoc.Save(path);
+            xDoc.Save(sPath);
             Exit();
         }
 
@@ -49,7 +58,7 @@ namespace Flammenwerfer
 
         public void Exit()
         {// when user is created, exit program
-            displayer.DumbInfoDisplay("Please press any key to close program");
+            cDisplayer.DumbInfoDisplay("Please press any key to close program");
             Console.ReadKey();
             Environment.Exit(0);
         }
@@ -61,56 +70,56 @@ namespace Flammenwerfer
         private void StudentIDNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xSID = xDoc.CreateElement("SID"); //<SID>
-            xSID.InnerText = displayer.ReadInfoDisplay("Student ID number: "); //Input SID data
+            xSID.InnerText = cDisplayer.ReadInfoDisplay("Student ID number: "); //Input SID data
             xStudent.AppendChild(xSID); //</SID>
         }
 
         private void StudentFNNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xFName = xDoc.CreateElement("FName"); //<FName>
-            xFName.InnerText = displayer.ReadInfoDisplay("Student first name: "); //Input FName data
+            xFName.InnerText = cDisplayer.ReadInfoDisplay("Student first name: "); //Input FName data
             xStudent.AppendChild(xFName); //</FName>
         }
 
         private void StudentLNNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xLName = xDoc.CreateElement("LName"); //<LName>
-            xLName.InnerText = displayer.ReadInfoDisplay("Student last name: "); //Input LName data
+            xLName.InnerText = cDisplayer.ReadInfoDisplay("Student last name: "); //Input LName data
             xStudent.AppendChild(xLName); //</LName>
         }
 
         private void CourseIDNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xCourseID = xDoc.CreateElement("CourseID"); //<CourseID>
-            xCourseID.InnerText = displayer.ReadInfoDisplay("ID of course: "); //Input CourseID data
+            xCourseID.InnerText = cDisplayer.ReadInfoDisplay("ID of course: "); //Input CourseID data
             xStudent.AppendChild(xCourseID); //</CourseID>
         }
 
         private void CourseNameNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xCourseName = xDoc.CreateElement("CourseName"); //<CourseName>
-            xCourseName.InnerText = displayer.ReadInfoDisplay("Name of course: "); //Input CourseName data
+            xCourseName.InnerText = cDisplayer.ReadInfoDisplay("Name of course: "); //Input CourseName data
             xStudent.AppendChild(xCourseName); //</CourseName>
         }
 
         private void SemesterNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xSem = xDoc.CreateElement("Semester"); //<Semester>
-            xSem.InnerText = displayer.ReadInfoDisplay("Semester: "); //Input Semester data
+            xSem.InnerText = cDisplayer.ReadInfoDisplay("Semester: "); //Input Semester data
             xStudent.AppendChild(xSem); //</Semester>
         }
 
         private void CourseTypeNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xCourseType = xDoc.CreateElement("CourseType"); //<CourseType>
-            xCourseType.InnerText = displayer.ReadInfoDisplay("type of course: "); //Input CourseType data
+            xCourseType.InnerText = cDisplayer.ReadInfoDisplay("type of course: "); //Input CourseType data
             xStudent.AppendChild(xCourseType); //</CourseType>
         }
 
         private void CourseGradeNode(XmlDocument xDoc, XmlNode xStudent)
         {
             XmlNode xCourseGrade = xDoc.CreateElement("CourseGrade"); //<CourseGrade>
-            xCourseGrade.InnerText = displayer.ReadInfoDisplay("Grade of course: "); //Input CourseGrade data
+            xCourseGrade.InnerText = cDisplayer.ReadInfoDisplay("Grade of course: "); //Input CourseGrade data
             xStudent.AppendChild(xCourseGrade); //</CourseGrade>
         }
 
